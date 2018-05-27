5308ee53efb98883da54be2fa86a9faea289f364
kellerman <kellerman.mota@itsstecnologia.com.br>
2018-05-27 15:48:47 -0300

    comentou codigo e otimizou usabilidade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Card } from './card.model';
+import { Card, CardColors } from './card.model';
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
@@ -8,19 +8,11 @@ import { ToastsManager } from 'ng2-toastr/ng2-toastr';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  verseIcon = "glyphicon glyphicon-question-sign";
-  cardValuesTemplate = [
-    "glyphicon glyphicon-heart", 
-    "glyphicon glyphicon-plane", 
-    "glyphicon glyphicon-leaf",
-    "glyphicon glyphicon-music", 
-    "glyphicon glyphicon-cloud", 
-    "glyphicon glyphicon-star"];
   cards: Card[] = [];
-  cardsSelected: Card[] = [];
+  turnedCards: Card[] = [];
   points: number;
   message: string;
-  error: Boolean;
+  twoCardsUnmatch: Boolean;
 
   constructor(public toastr: ToastsManager, vcr: ViewContainerRef) {
     this.toastr.setRootViewContainerRef(vcr);
@@ -30,78 +22,123 @@ export class AppComponent implements OnInit {
     this.resetGame();
   }
 
+  /**
+   * 
+   * this method is called when the user touch the card. 
+   * 
+   * */
   turn(card: Card) {
+    // if the user click on one card that's already turned and validated, then do nothing
     if (card.disabled) return;
-    this.validateSelectedCardsOnError();
-    card.showValue = !card.showValue;
-    if (card.showValue) {
-      this.cardsSelected.push(card);
+    // if two cards already turned and unmatch
+    this.validateTwoCardsUnmatch();
+    // if show front value, add to turnedCards, else remove from turnedCards
+    card.showFrontValue = !card.showFrontValue;
+    if (card.showFrontValue) {
+      this.turnedCards.push(card);
     } else {
-      this.cardsSelected = this.cardsSelected.filter(c => c != card);
-    }
-    this.validateSelectedCards();
+      this.turnedCards = this.turnedCards.filter(c => c != card);
+    }    
+    this.validateTurnedCards();    
     this.validateEndOfGame();
   }
 
-  validateSelectedCards() {
-    if (this.cardsSelected.length == 2) {
-      if (this.cardsSelected[0].value == this.cardsSelected[1].value) {
+  /**
+   * 
+   * if two cards already turned, then validate the values
+   * 
+   */
+  private validateTurnedCards() {
+    if (this.turnedCards.length == 2) {
+      if (this.turnedCards[0].frontValue == this.turnedCards[1].frontValue) {
         this.message = "Match";
-        this.cardsSelected.forEach(card => {
+        // important: the arrays 'cards' and 'turnedCards' holds the same references. 
+        // Then, the changes on 'turnedCards' will reflect on 'cards' too, 
+        // even if the 'turnedCards' is cleaned after
+        this.turnedCards.forEach(card => {
           card.disabled = true;
-          card.setMatchColor();
+          card.color = CardColors.match;
         })
-        this.cardsSelected = [];
+        // clean the array
+        this.turnedCards = [];
         this.points++;
         this.showSuccess("Match");
       } else {
-        this.message = "Not Match";
+        this.message = "Unmatch";
         this.points--;
-        this.showError("Not Match");
-        this.error = true;
-        this.cardsSelected.forEach(c => c.setNotMatchColor());
+        this.showError("Unmatch");
+        this.twoCardsUnmatch = true;
+        this.turnedCards.forEach(c => c.color = CardColors.unmatch);
       }
     }
   }
 
-  validateSelectedCardsOnError() {
-    if (this.error) {
-      this.cardsSelected.forEach(card => {
-        card.showValue = false;
-        card.setDefaultColor();
+  /**
+   * 
+   * reset state if two cards turned has unmatch values
+   * 
+   */
+  private validateTwoCardsUnmatch() {
+    if (this.twoCardsUnmatch) {
+      // important: the arrays cards and turnedCards holds the same references. 
+      // Then, the changes on turnedCards will reflect on cards too, 
+      // even if the turnedCards is cleaned after
+      this.turnedCards.forEach(card => {
+        card.showFrontValue = false;
+        card.color = CardColors.default;
       })
-      this.cardsSelected = [];
-      this.error = false;
+      // clean the array
+      this.turnedCards = [];
+      this.twoCardsUnmatch = false;
     }
   }
-
-  validateEndOfGame() {
-    if (this.cards.filter(card => !card.showValue).length == 0) {
+  /**
+   * 
+   * if all cards are turned, then is the end of game 
+   * 
+   */
+  private validateEndOfGame() {
+    if (this.cards.filter(card => !card.showFrontValue).length == 0) {
       this.message = "End of the game!";
+      if(confirm(`End of the game.\nYou make [${this.points} point(s)].\n\nDo you want try again?`)){
+        this.resetGame();
+      }
     }
   }
 
+  /**
+   * 
+   * this method init and reset the game.
+   * 
+   */
   resetGame() {
     this.cards = [];
-    this.cardsSelected = [];
-    let cardValues = this.cardValuesTemplate.concat(this.cardValuesTemplate);
-    cardValues = this.shuffle(cardValues);
-    cardValues.forEach(val => this.cards.push(new Card(val)))
+    this.turnedCards = [];
+    // duplicate all front values for match/unmath test works later
+    let duplicateFrontValues = Card.templateValues().concat(Card.templateValues());
+    // shuffle the values and create one card with every value
+    this.shuffle(duplicateFrontValues).forEach(val => this.cards.push(new Card(val)))
     this.points = 0;
     this.message = "Choose a card";
-    this.error = false;
+    this.twoCardsUnmatch = false;
   }
 
-  showSuccess(msg: string) {
+  private showSuccess(msg: string) {
     this.toastr.success(msg, 'Success!');
   }
 
-  showError(msg: string) {
+  private showError(msg: string) {
     this.toastr.error(msg, 'Oops!');
   }
 
-  // copy/paste from stackoverflow...rsrsrs
-  shuffle(array) {
+  /**
+   * 
+   * shuffle the values before set to the cards
+   * 
+   * @param array 
+   * 
+   */
+  private shuffle(array) {
     let currentIndex = array.length;
 
     // While there remain elements to shuffle...
--- src/app/card.model.ts
+++ src/app/card.model.ts
@@ -1,25 +1,50 @@
+/**
+ * enumerated state colors 
+ * 
+ * @author Kellerman
+ *  
+ */
+export enum CardColors {
+    match = '#58C565',
+    unmatch = '#C54023',
+    default = '#EEB422'    
+};
+
+/**
+ * It's a card model and represent
+ * every card on the view
+ * 
+ * @author Kellerman
+ * 
+ */
 export class Card {
-    value: string;
-    showValue: Boolean;
+    frontValue: string;
+    verseValue: string;
+    showFrontValue: Boolean;
     disabled: Boolean;
     color: string;
 
-    constructor(value: string) {
-        this.value = value;
-        this.showValue = false;
+    constructor(frontValue: string) {
+        this.verseValue = "glyphicon glyphicon-question-sign";
+        this.frontValue = frontValue;
+        this.showFrontValue = false;
         this.disabled = false;
-        this.setDefaultColor();
+        this.color = CardColors.default;
     }
 
-    setNotMatchColor() {
-        this.color = '#C54023';
-    }
-
-    setDefaultColor() {
-        this.color = '#EEB422';
-    }
-
-    setMatchColor() {
-        this.color = '#58C565';
+    /**
+     * 
+     * static method that return the icons of every card
+     * 
+     */
+    static templateValues(): string[] {
+        return [
+            "glyphicon glyphicon-heart",
+            "glyphicon glyphicon-plane",
+            "glyphicon glyphicon-leaf",
+            "glyphicon glyphicon-music",
+            "glyphicon glyphicon-ice-lolly-tasted",
+            "glyphicon glyphicon-star"
+        ];
     }
 }
